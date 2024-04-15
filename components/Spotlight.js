bd8741e07ff84c6cdcd569464786b789175ad1e2
Volker Boehm <boehm.volker@googlemail.com>
2024-04-15 12:16:52 +0200

    add userstory 2

--- components/Spotlight.js
+++ components/Spotlight.js
@@ -1,7 +1,22 @@
-export default function Spotlight() {
+import Image from "next/image";
+import React from "react";
+
+export default function Spotlight({ artPieces }) {
+  const randomIndex = Math.floor(Math.random() * artPieces.length);
+  const spotlightArtPiece = artPieces[randomIndex];
+  const reduceWidth = spotlightArtPiece.dimensions.width * 0.2;
+  const reduceHeight = spotlightArtPiece.dimensions.height * 0.2;
+
   return (
-    <>
+    <div>
       <h2>Spotlight</h2>
-    </>
+      <Image
+        width={reduceWidth}
+        height={reduceHeight}
+        alt={`${spotlightArtPiece.name} by ${spotlightArtPiece.artist}`}
+        src={spotlightArtPiece.imageSource}
+      />
+      <p>Artist: {spotlightArtPiece.artist}</p>
+    </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function HomePage() {
 
   return (
     <div>
-      <Spotlight />
+      <Spotlight artPieces={data} />
       <ArtPieces artPieces={data} />
     </div>
   );
